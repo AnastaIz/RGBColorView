8a9df60a0ec454c04652e74d3a75d1fd1dd10a80
Anastasia Izmaylova <abulatova1992@gmail.com>
2021-12-12 15:27:20 +0300

    View with changing color

--- RGBColorView/ViewController.swift
+++ RGBColorView/ViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redValue: UILabel!
+    @IBOutlet weak var greenValue: UILabel!
+    @IBOutlet weak var blueValue: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSlider.value = 1
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.minimumTrackTintColor = .red
+        redValue.text = String(redSlider.value)
+        
+        greenSlider.value = 1
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenSlider.minimumTrackTintColor = .green
+        greenValue.text = String(greenSlider.value)
+        
+        blueSlider.value = 1
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueSlider.minimumTrackTintColor = .blue
+        blueValue.text = String(blueSlider.value)
     }
 
-
+    @IBAction func redSliderAction() {
+        redValue.text = String(redSlider.value)
+      fillViewWithColor()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValue.text = String(greenSlider.value)
+        fillViewWithColor()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueValue.text = String(blueSlider.value)
+        fillViewWithColor()
+    }
+    
+    func fillViewWithColor (){
+        let redSliderValue = CGFloat(redSlider.value)
+        let greenSliderValue = CGFloat(greenSlider.value)
+        let blueSliderValue = CGFloat(blueSlider.value)
+        
+        let color = UIColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: 1)
+        
+        colorView.backgroundColor = color
+    }
 }
 
